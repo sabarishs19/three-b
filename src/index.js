eb5f05520ae910d0c965fe8c7a2323c2af38b6f1
sabarishs19 <84761863+sabarishs19@users.noreply.github.com>
2021-12-13 11:54:11 +0530

    css

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
-
+import "./styles.css";
 import App from "./App";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <StrictMode>
+  <>
     <App />
-  </StrictMode>,
-  rootElement
+    <div className="overlay">
+      <a className="left" href="">
+        S
+      </a>
+      <a className="right" href="">
+        csb
+      </a>
+
+      <h2>SPACESURF</h2>
+    </div>
+  </>,
+  document.getElementsByTagName("root")
 );

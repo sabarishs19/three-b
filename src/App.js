ee164f4dfc1801755b7db13d11add6f92aef8fc9
sabarishs19 <84761863+sabarishs19@users.noreply.github.com>
2021-12-13 11:40:06 +0530

    app

--- src/App.js
+++ src/App.js
@@ -56,3 +56,57 @@ export default function App() {
     </Canvas>
   );
 }
+
+function Bubbles() {
+  const ref = useRef();
+  useFrame(
+    (state, delta) =>
+      void (ref.current.rotation.y = MathUtils.damp(
+        ref.current.rotation.y,
+        (-state.mouse.x * Math.PI) / 6,
+        0.75,
+        delta
+      ))
+  );
+  return (
+    <Instances
+      limit={particles.length}
+      ref={ref}
+      castShadow
+      receiveShadow
+      position={[0, 10, 0]}
+    >
+      <sphereGeometry args={[1, 32, 32]} />
+      <meshStandardMaterial roughness={0} color="#f0f0f0" />
+      {particles.map((data, i) => (
+        <Bubble key={i} {...data} />
+      ))}
+    </Instances>
+  );
+}
+
+function Bubble({ factor, speed, xFactor, yFactor, zFactor }) {
+  const ref = useRef();
+  useFrame((state) => {
+    const t = factor + state.clock.elapsedTime * (speed / 2);
+    ref.current.scale.setScalar(Math.max(1.5, Math.cos(t) * 5));
+    ref.current.position.set(
+      Math.cos(t) +
+        Math.sin(t * 1) / 10 +
+        xFactor +
+        Math.cos((t / 10) * factor) +
+        (Math.sin(t * 1) * factor) / 10,
+      Math.sin(t) +
+        Math.cos(t * 2) / 10 +
+        yFactor +
+        Math.sin((t / 10) * factor) +
+        (Math.cos(t * 2) * factor) / 10,
+      Math.sin(t) +
+        Math.cos(t * 2) / 10 +
+        zFactor +
+        Math.cos((t / 10) * factor) +
+        (Math.sin(t * 3) * factor) / 10
+    );
+  });
+  return <Instance ref={ref} />;
+}

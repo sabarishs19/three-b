fce59ad3d35ae6eb07208c574360ca74dccbd3ac
sabarishs19 <84761863+sabarishs19@users.noreply.github.com>
2021-12-13 11:32:19 +0530

    return

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./styles.css";
 import { MathUtils } from "three";
 import { useRef, Suspense } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
 import {
   Instances,
   Instance,
   Environment,
   ContactShadows
-} from "@recact-three/drei";
+} from "@react-three/drei";
 import { EffectComposer, SSAO } from "@react-three/postprocessing";
 
 const particles = Array.from({ length: 150 }, () => ({
@@ -19,9 +20,39 @@ const particles = Array.from({ length: 150 }, () => ({
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <Canvas
+      shadows
+      dpr={[1, 2]}
+      gl={{ alpha: false, antialias: false }}
+      camera={{ fov: 75, position: [0, 0, 60], near: 10, far: 150 }}
+    >
+      <color attach="background" args={["#f0f0f0"]} />
+      <fog attacg="fog" args={["red", 60, 110]} />
+      <ambientLight intensity={1.5} />
+      <pointLight position={[100, 10, -50]} intensity={20} castShadow />
+      <pointLight position={[-100, -100, -100]} intensity={10} color="red" />
+      <Bubbles />
+      <ContactShadows
+        rotation={[Math.PI / 2, 0, 0]}
+        position={[0, -30, 0]}
+        opacity={0.6}
+        width={130}
+        height={130}
+        blur={1}
+        far={40}
+      />
+      <EffectComposer multisampling={0}>
+        <SSAO
+          samples={31}
+          radius={10}
+          intensity={20}
+          luminanceInfluence={0.1}
+          color="red"
+        />
+      </EffectComposer>
+      <Suspense fallback={null}>
+        <Environment preset="city" />
+      </Suspense>
+    </Canvas>
   );
 }
